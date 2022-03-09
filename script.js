731d45e9fec37ab1e056dd4428978b20df49f027
ajitbubu <ajitbubu@gmail.com>
2022-03-09 07:23:27 -0600

    Contact us section added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('Its working')
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
-	setTheme('light')
+	setTheme('blue')
 }else{
 	setTheme(theme)
 }
@@ -38,3 +38,67 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+
+// Initialize Firebase (ADD YOUR OWN DATA)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: Replace with your app's Firebase project configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAjXQdlXEqP3xFg72HNQgA5WY3RFS_E82c",
+    authDomain: "aj-portfolio-8bd12.firebaseapp.com",
+    databaseURL: "https://aj-portfolio-8bd12-default-rtdb.firebaseio.com",
+    projectId: "aj-portfolio-8bd12",
+    storageBucket: "aj-portfolio-8bd12.appspot.com",
+    messagingSenderId: "916903533637",
+    appId: "1:916903533637:web:6e94dd747c87efeb07d72c",
+    measurementId: "G-5PM4YBXGW2"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, email, phone, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email:email,
+      phone:phone,
+      message:message
+    });
+  }
